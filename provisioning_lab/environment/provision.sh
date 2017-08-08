e0fd64d77452fa8745be3fb32f0af146a1ae2c9c
Fane <fane247@gmail.com>
2017-08-08 11:09:38 +0100

    added all files

---
+++ provisioning_lab/environment/provision.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-get update
+sudo apt-get install ruby2.4 -y
+sudo gem install bundler
+sudo gem install rake
+sudo apt-get install ruby2.4-dev -y	
+sudo apt-get install build-essential -y	
+sudo apt-get install libxml2-dev libgmp-dev zlib1g-dev -y
+sudo gem install rails
+sudo apt-get install nginx -y
+sudo apt-get install nodejs -y
+
+
